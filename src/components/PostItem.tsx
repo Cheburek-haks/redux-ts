fca45078d4c1ab68f1656839eb5885b6ce7d9b96
Cheburek <islam2_14@mail.ru>
2023-03-30 17:11:22 +0300

    first commit

---
+++ src/components/PostItem.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { IPost } from "../models/IPost";
+
+interface PostItemProps {
+  post: IPost;
+}
+
+const PostItem: React.FC<PostItemProps> = ({ post }) => {
+  return (
+    <div className="post">
+      {post.id}.{post.title}
+      <button>Delete</button>
+    </div>
+  );
+};
+
+export default PostItem;
---
+++ src/components/Postcontainer.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { postAPI } from "../services/PostService";
+import PostItem from "./PostItem";
+
+const PostContainer = () => {
+  const { data: posts } = postAPI.useFetchAllPostsQuery(5);
+  return (
+    <div>
+      <div className="post__list">
+        {posts?.map((post) => (
+          <PostItem post={post} />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default PostContainer;
